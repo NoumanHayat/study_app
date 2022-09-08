fe4eaf051dd168c94b782cc58eefa5ce21dae145
Nouman Hayat <muhammadnoumanhayat@gmail.com>
2022-09-08 17:07:42 +0500

    Started Screen

--- src/screens/Dashboard/home.tsx
+++ src/screens/Dashboard/home.tsx
@@ -13,6 +13,7 @@ const App = (props) => {
   //       borderTopEndRadius: 45,
   const [process, setProcess] = useState('01');
   const [showModal, setModal] = useState(false);
+  const [step, setStep] = useState([1, 2, 3, 4]);
   //       // bottom: 35, 
   return (
     <Block safeScroll flex={1}  >
@@ -45,67 +46,68 @@ const App = (props) => {
       </Block>
       <Block flex={1} margin={sizes.sm}>
         <Text h5 marginBottom={sizes.sm}>Batch 1</Text>
-        {/* <FlatList
-          keyExtractor={(index) => `${index}`}
-          data={['01', '02', '03', '04', '05']}
-          renderItem={({ item }) => (
-            <Button
-              marginBottom={sizes.sm}
-              onPress={() => {
-                setProcess(item);
-                setModal(false);
-              }}>
-              <Text p  semibold transform="uppercase">
-                {item}
-              </Text>
-            </Button>
-          )}
-        /> */}
-        <Block card row>
-          <Block flex={1}><Text>Step 1</Text></Block>
-          <Block flex={1} row>
-            <Button
-              flex={1}
-              gradient={gradients.divider}
-              style={{ margin: 5 }}
-              onPress={() => {
-                alert(
-                  ' <Button flex={1} gradient={gradients.custom5} marginBottom={sizes.base} > <Text white bold transform="uppercase"> title</Text></Button>',
-                );
-              }}>
-              <Text bold transform="uppercase">
-                <AntDesign name="pause" size={24} color="black" />
-              </Text>
-            </Button>
-            <Button
-              flex={1}
-              gradient={gradients.divider}
-              style={{ margin: 5 }}          // style={{ margin: 5 }}
-              onPress={() => {
-                alert(
-                  ' <Button flex={1} gradient={gradients.custom5} marginBottom={sizes.base} > <Text white bold transform="uppercase"> title</Text></Button>',
-                );
-              }}>
-              <Text bold transform="uppercase">
-                <AntDesign name="rightcircle" size={20} color="black" />
-              </Text>
-            </Button>
-            <Button
-              flex={1}
-              style={{ margin: 5 }}
-              gradient={gradients.divider}
-              // style={{ margin: 5 }}
-              onPress={() => {
-                alert(
-                  ' <Button flex={1} gradient={gradients.custom5} marginBottom={sizes.base} > <Text white bold transform="uppercase"> title</Text></Button>',
-                );
-              }}>
-              <Text bold transform="uppercase">
-                <AntDesign name="delete" size={24} color="black" />
-              </Text>
-            </Button>
-          </Block>
-        </Block>
+        {step.map((item) => {
+          return (
+            <Block card row margin={4} key={item}>
+              <Block flex={1} style={{ justifyContent: 'center' }}><Text h5>Step 1</Text></Block>
+              <Block flex={1} row>
+                <Button
+                  flex={1}
+                  gradient={gradients.divider}
+                  style={{ margin: 5 }}
+                  onPress={() => {
+                    alert(
+                      ' <Button flex={1} gradient={gradients.custom5} marginBottom={sizes.base} > <Text white bold transform="uppercase"> title</Text></Button>',
+                    );
+                  }}>
+                  <Text bold transform="uppercase">
+                    <AntDesign name="pause" size={24} color="black" />
+                  </Text>
+                </Button>
+                <Button
+                  flex={1}
+                  gradient={gradients.divider}
+                  style={{ margin: 5 }}          // style={{ margin: 5 }}
+                  onPress={() => {
+                    alert(
+                      ' <Button flex={1} gradient={gradients.custom5} marginBottom={sizes.base} > <Text white bold transform="uppercase"> title</Text></Button>',
+                    );
+                  }}>
+                  <Text bold transform="uppercase">
+                    <AntDesign name="rightcircle" size={20} color="black" />
+                  </Text>
+                </Button>
+                <Button
+                  flex={1}
+                  style={{ margin: 5 }}
+                  gradient={gradients.divider}
+                  // style={{ margin: 5 }}
+                  onPress={() => {
+                    alert(
+                      ' <Button flex={1} gradient={gradients.custom5} marginBottom={sizes.base} > <Text white bold transform="uppercase"> title</Text></Button>',
+                    );
+                  }}>
+                  <Text bold transform="uppercase">
+                    <AntDesign name="delete" size={24} color="black" />
+                  </Text>
+                </Button>
+              </Block>
+            </Block>
+          )
+        })}
+        <Button
+          flex={1}
+          gradient={gradients.info}
+          margin={sizes.base}
+          onPress={() => {
+            alert(
+              ' <Button flex={1} gradient={gradients.custom5} marginBottom={sizes.base} > <Text white bold transform="uppercase"> title</Text></Button>',
+            );
+          }}>
+          <Text white bold transform="uppercase">
+            New Batch
+          </Text>
+        </Button>
       </Block>
       <Modal visible={showModal} onRequestClose={() => setModal(false)}>
         <FlatList
